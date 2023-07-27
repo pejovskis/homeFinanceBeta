ce225a080577f995b904961ff42ab4b0a92337e6
sashko <pejovskis2@gmail.com>
2023-07-28 00:35:58 +0200

    First Working navi, new menu and btn.

--- app/src/main/java/com/example/homefinancebeta/AddExpenses.java
+++ app/src/main/java/com/example/homefinancebeta/AddExpenses.java
@@ -3,10 +3,14 @@ package com.example.homefinancebeta;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +63,26 @@ public class AddExpenses extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_expenses, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_expenses, container, false);
+
+        Button btnBack = view.findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToPreviousMenu();
+            }
+        });
+
+
+        return view;
+    }
+
+    private void navigateToPreviousMenu() {
+
+        NavController navController = NavHostFragment.findNavController(this);
+
+        navController.navigateUp();
+
     }
 }
