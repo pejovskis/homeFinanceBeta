78a1eebfb962c7ca2bf382e02d9ed159d227323e
sashko <pejovskis2@gmail.com>
2023-07-30 13:51:27 +0200

    Added Table, needed Functionality

--- app/src/main/java/com/example/homefinancebeta/ShowExpenses.java
+++ app/src/main/java/com/example/homefinancebeta/ShowExpenses.java
@@ -1,15 +1,26 @@
 package com.example.homefinancebeta;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -66,6 +77,8 @@ public class ShowExpenses extends Fragment {
 
         Button btnBack = view.findViewById(R.id.btnBack);
 
+        TableLayout tableLayoutExpenses = view.findViewById(R.id.tableLayoutExpenses);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,7 +86,95 @@ public class ShowExpenses extends Fragment {
             }
         });
 
+        // Retrieve and display the expenses
+        retrieveAndDisplayExpenses(view);
+
         return view;
+    }
+
+    private void retrieveAndDisplayExpenses(View view) {
+        DatabaseReference expensesRef = FirebaseDatabase.getInstance().getReference("expenses");
+        expensesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Clear any previous data from the table
+                TableLayout tableLayoutExpenses = view.findViewById(R.id.tableLayoutExpenses);
+                //tableLayoutExpenses.removeAllViews();
+
+                if (dataSnapshot.exists()) {
+                    // If data exists, loop through the children of the "expenses" node
+                    for (DataSnapshot expenseSnapshot : dataSnapshot.getChildren()) {
+                        // Retrieve the expense object from the snapshot
+                        Expense expense = expenseSnapshot.getValue(Expense.class);
+
+                        // Add the expense data to the table
+                        addExpenseToTable(tableLayoutExpenses, expense);
+                    }
+                } else {
+                    // If no data exists, add the table headers and show a message
+                    // addTableHeaders(tableLayoutExpenses);
+                    addNoDataMessage(tableLayoutExpenses);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle any errors that may occur while retrieving data
+            }
+        });
+    }
+
+    private void addNoDataMessage(TableLayout tableLayout) {
+        TableRow row = new TableRow(requireContext());
+
+        TextView tvNoData = createTextView("No expenses found.");
+        tvNoData.setGravity(Gravity.CENTER);
+        tvNoData.setTextColor(Color.RED);
+
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.span = 6; // Span the message across all columns
+
+        row.addView(tvNoData, layoutParams);
+
+        // Add the TableRow to the TableLayout
+        tableLayout.addView(row);
+    }
+
+    private void addExpenseToTable(TableLayout tableLayout, Expense expense) {
+        // Create a new TableRow
+        TableRow row = new TableRow(requireContext());
+
+        // Create TextViews to display each attribute of the expense
+        TextView tvId = createTextView(expense.getId());
+        TextView tvWhere = createTextView(expense.getWhere());
+        TextView tvEssentials = createTextView(expense.getEssentials());
+        TextView tvCategory = createTextView(expense.getCategory());
+        TextView tvDate = createTextView(expense.getDate());
+        TextView tvPrice = createTextView(expense.getPrice());
+
+        // Add TextViews to the TableRow
+        row.addView(tvId);
+        row.addView(tvWhere);
+        row.addView(tvEssentials);
+        row.addView(tvCategory);
+        row.addView(tvDate);
+        row.addView(tvPrice);
+
+        // Add the TableRow to the TableLayout
+        tableLayout.addView(row);
+    }
+
+    private TextView createTextView(String text) {
+        TextView textView = new TextView(requireContext());
+        textView.setText(text);
+        textView.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+        ));
+        return textView;
     }
 
     // Back Btn
